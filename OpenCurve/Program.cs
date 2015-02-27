9af35a596baf18b3a06a1c7c7ab775fe901d953e
Sheldar <kewin.polok@gmail.com>
2015-02-27 17:12:11 +0100

    Initialization of monogame project.

---
+++ OpenCurve/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenCurve
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new OpenCurveGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
